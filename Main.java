e1ee3973475a12f227f2af3d7e39c88ca9f7b141
Brian Milian <600021305@fjuhsd.org>
2021-02-12 23:06:07 +0000

    updated line 64

--- Main.java
+++ Main.java
@@ -8,6 +8,13 @@ public class Main
 	{
 		
             int[] nums = {7,28,496,1111,199,201,17};
+            for( int num : nums ) 
+            { Number one = new Number( num ); 
+            if ( one.isOdd() ) System.out.print( one + " is odd.\n" ); 
+            else System.out.print( one + " is not odd.\n" ); 
+            
+            if ( one.isPerfect() ) System.out.print( one + " is perfect.\n" ); 
+            else System.out.print( one + " is not perfect.\n" ); }
             //see readme file for additional code
            
 	}
--- Number.java
+++ Number.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Brian Milian
 
 
 //write the Number class
@@ -9,20 +9,71 @@
 //to guide you
 
 //header for Number class
-
+public class Number
+{
 //private instance variable (must be an Integer)
+private Integer num;
 
 //constructor
+public Number()
+{
+  num = 0;
+}
 
+public Number(int x)
+{
+  num = x;
+}
 
 //getter method
-
+public Integer getNum()
+{
+  return num;
+}
 
 //setter method
-
+public void setNum(int x)
+{
+  num = x;
+}
 //isOdd method
+public boolean isOdd()
+{
+  if(num % 2 == 0)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
 
 //isPerfect method
+public boolean isPerfect()
+{
+  int x = 0;
+  for(int y = 1; y < num; y = y + 1)
+  {
+    if(num % y == 0)
+    {
+      x += y;
+    }
+  }
 
+  if(num == x)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
 
 //toString
+public String toString()
+{
+  return ""+num;
+}
+}
